04a2383e0ef3148bb583ce850b326296ef06f1dd
pt-tadaoka <pt-tadaoka@jokerpiece.co.jp>
2015-04-09 17:54:17 +0900

    aar0.0.9のエラー修正->aar0.0.10

---
+++ android.PieceCore/app/src/main/java/jp/co/jokerpiece/piecebase/InfomationListFragment.java
@@ -0,0 +1,246 @@
+package jp.co.jokerpiece.piecebase;
+
+import java.util.ArrayList;
+
+import jp.co.jokerpiece.piecebase.data.NewsListData;
+import jp.co.jokerpiece.piecebase.data.NewsListData.NewsData;
+import jp.co.jokerpiece.piecebase.util.AppUtil;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class InfomationListFragment extends Fragment implements OnItemClickListener {
+	public static final String TAG = "InfomationListFragment";
+
+	private ListView lv;
+
+	private InfoListAdapter adapter;
+	private NewsListData data;
+	private int type;
+
+	/**
+	 * コンストラクタ
+	 */
+	public InfomationListFragment() {
+		super();
+	}
+
+	/**
+	 * コンストラクタ
+	 */
+	public InfomationListFragment(NewsListData data, int type) {
+		super();
+		this.data = data;
+		this.type = type;
+	}
+
+	@SuppressLint("InflateParams")
+	@Override
+	  public View onCreateView(LayoutInflater inflater,
+	    ViewGroup container,
+	    Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_infomation_list, null);
+
+		lv = (ListView) rootView.findViewById(R.id.infomation_list_view);
+		if(savedInstanceState != null){
+			data = (NewsListData)savedInstanceState.getSerializable("InfomationDataList");
+			type = savedInstanceState.getInt("InfomationType");
+		}
+		if (data != null) {
+			adapter = new InfoListAdapter(getActivity(), getDataWithType(data.data_list));
+			lv.setAdapter(adapter);
+		}
+		lv.setOnItemClickListener(this);
+
+	    return rootView;
+	}
+	public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putSerializable("InfomationDataList", data);
+        outState.putInt("InfomationType", type);
+    }
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+		if (data == null) { return; }
+
+		ArrayList<NewsData> newsList = getDataWithType(data.data_list);
+		NewsData newsData = newsList.get(position);
+
+		switch (newsData.type) {
+		case NewsListData.NEWS_DATA_TYPE_INFOMATION + "":
+			// お知らせ詳細画面に遷移
+			FragmentManager fmInfo = getParentFragment().getFragmentManager();
+			FragmentTransaction ftInfo = fmInfo.beginTransaction();
+			ftInfo.addToBackStack(null);
+			InfomationSyosaiFragment fragmentInfo = new InfomationSyosaiFragment();
+			Bundle bundleInfo = new Bundle();
+			bundleInfo.putString("newsId", newsData.news_id);
+			fragmentInfo.setArguments(bundleInfo);
+			ftInfo.replace(R.id.fragment, fragmentInfo);
+			ftInfo.commit();
+			break;
+		case NewsListData.NEWS_DATA_TYPE_FLYER + "":
+			// フライヤー画面に遷移
+			MainBaseActivity.tabHost.setCurrentTab(AppUtil.getPosition("Flyer"));
+//			FragmentManager fmFlyer = getParentFragment().getFragmentManager();
+//			FragmentTransaction ftFlyer = fmFlyer.beginTransaction();
+//			ftFlyer.addToBackStack(null);
+//			FlyerFragment fragmentFlyer = new FlyerFragment();
+//			try {
+//				Bundle bundleFlyer = new Bundle();
+//				bundleFlyer.putInt("flyer_ID", Integer.parseInt(newsData.id));
+//				fragmentFlyer.setArguments(bundleFlyer);
+//			} catch (NumberFormatException e) {
+//				e.printStackTrace();
+//			}
+//			ftFlyer.replace(R.id.fragment, fragmentFlyer);
+//			ftFlyer.commit();
+			break;
+		case NewsListData.NEWS_DATA_TYPE_COUPON + "":
+			// クーポン画面に遷移
+			MainBaseActivity.tabHost.setCurrentTab(AppUtil.getPosition("Coupon"));
+//			FragmentManager fmCoupon = getParentFragment().getFragmentManager();
+//			FragmentTransaction ftCoupon = fmCoupon.beginTransaction();
+//			ftCoupon.addToBackStack(null);
+//			CouponFragment fragmentCoupon = new CouponFragment();
+//			Bundle bundleCoupon = new Bundle();
+//			bundleCoupon.putString("coupon_id", newsData.id);
+//			fragmentCoupon.setArguments(bundleCoupon);
+//			ftCoupon.replace(R.id.fragment, fragmentCoupon);
+//			ftCoupon.commit();
+			break;
+		}
+	}
+
+	/**
+	 * タイプによってデータを選別する。
+	 */
+	public ArrayList<NewsData> getDataWithType(ArrayList<NewsData> list) {
+		ArrayList<NewsData> ret = new ArrayList<NewsData>();
+		if (list != null) {
+			for (int i = 0; i < list.size(); i++) {
+				NewsData newsData = list.get(i);
+				switch (type) {
+				case NewsListData.NEWS_DATE_TYPE_ALL:
+					ret.add(newsData);
+					break;
+				case NewsListData.NEWS_DATA_TYPE_INFOMATION:
+					if (newsData.type.equals(NewsListData.NEWS_DATA_TYPE_INFOMATION + "")) {
+						ret.add(newsData);
+					}
+					break;
+				case NewsListData.NEWS_DATA_TYPE_FLYER:
+					if (newsData.type.equals(NewsListData.NEWS_DATA_TYPE_FLYER + "")) {
+						ret.add(newsData);
+					}
+					break;
+				case NewsListData.NEWS_DATA_TYPE_COUPON:
+					if (newsData.type.equals(NewsListData.NEWS_DATA_TYPE_COUPON + "")) {
+						ret.add(newsData);
+					}
+					break;
+				}
+			}
+		}
+		return ret;
+	}
+
+	/**
+	 * アダプタ
+	 */
+	public class InfoListAdapter extends BaseAdapter {
+		protected Context context;
+		protected ArrayList<NewsData> list;
+
+		public InfoListAdapter(Context context, ArrayList<NewsData> list) {
+			this.context = context;
+			setList(list);
+		}
+
+		public Context getContext() {
+			return this.context;
+		}
+
+		public void setList(ArrayList<NewsData> list) {
+			this.list = list;
+			notifyDataSetChanged();
+		}
+
+		@Override
+		public int getCount() {
+			return list.size();
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return list.get(position);
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
+
+		@SuppressLint("InflateParams")
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			TextView tvTitle;
+			ImageView ivInfo;
+			View v = convertView;
+
+			if (v == null) {
+				LayoutInflater inflater =
+						(LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+				v = inflater.inflate(R.layout.row_informationlist, null);
+			}
+
+			NewsData data = (NewsData)getItem(position);
+			if (data != null) {
+				tvTitle = (TextView) v.findViewById(R.id.tv_title);
+				ivInfo = (ImageView) v.findViewById(R.id.iv_info);
+
+				String title = data.title;
+				String type = data.type;
+
+				if (title != null) {
+					tvTitle.setText(title);
+				}
+
+				if (type != null) {
+					switch (type) {
+					case "1":
+						setImageResource(ivInfo, R.drawable.news_infomation);
+						break;
+					case "2":
+						setImageResource(ivInfo, R.drawable.news_shopping);
+						break;
+					case "3":
+						setImageResource(ivInfo, R.drawable.news_coupon);
+						break;
+					}
+				}
+			}
+
+			return v;
+		}
+
+		public void setImageResource(ImageView iv, int resId) {
+			iv.setVisibility(View.VISIBLE);
+			iv.setImageResource(resId);
+		}
+
+	}
+
+}
---
+++ android.PieceCore/app/src/main/java/jp/co/jokerpiece/piecebase/config/Config.java
@@ -0,0 +1,142 @@
+package jp.co.jokerpiece.piecebase.config;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import jp.co.jokerpiece.piecebase.R;
+import jp.co.jokerpiece.piecebase.data.MenuData;
+import jp.co.jokerpiece.piecebase.util.App;
+
+import android.content.Context;
+
+public class Config {
+    // --- staticイニシャライザで定数を設定(ここから) --- //
+    public static final String APP_ID;
+    public static final String APP_KEY;
+    public static final int SPLASH_TIME;
+//  public static String APP_ID = "pieceSample";
+//  public static final String APP_ID = "pushColor";
+//	public static final String APP_ID = "otonagokoro";
+//	public static final String APP_ID = "kodomogokoro";
+    // --- staticイニシャライザで定数を設定(ここまで) --- //
+
+	//新大人ゴコロ本番
+	public static final String SERVER_URL = "http://jokapi.jp/manager/html/xml/";
+
+	//大人ゴコロ本番
+	//public static final String SERVER_URL = "http://jokapi.jp/PieceProxy/service/api/shop/kodomogokoro/";
+	//大人ゴコロステージング
+	//public static final String SERVER_URL = "http://home.noumin.net/PieceProxy/service/api/shop/kodomogokoro/";
+	//public static final String SERVER_URL = "http://192.168.77.68/piece/html/xml/";
+
+	//http://192.168.77.68/piece/html/xml/device_token/index.php
+	//スタブ
+	//public static final String SERVER_URL = "http://192.168.77.83:8888/pieceSever/";
+
+	//デバッグ用UUID デバッグではない場合nullに;
+	public static final String getDebugUUID = "jokerpiece-test-uuid";
+	//public static final String getDebugUUID = null;
+
+	//サーバー接続までのタイムアウト時間
+	public static final int connectionTimeOut = 5000;
+
+	//サーバー接続後の読み込みタイムアウト時間
+	public static final int readTimeOut = 30000;
+
+	// プッシュ通知で必要なフィールド(ここから)
+	public static final String PROJECT_ID = "367759414941";	//プロジェクトIDを設定する
+    public static final String EXTRA_MESSAGE = "message";
+    public static final String PROPERTY_REG_ID = "registration_id";
+    public static final String PROPERTY_APP_VERSION = "appVersion";
+    public final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+
+    // プッシュ通知で必要なフィールド(ここまで)
+	//検索機能対応
+	public static final boolean SEARCH_PERMISSION = true;
+	public static final String USER_NAME = SERVER_URL + "home_list/";
+	public static final String SENDID_NEWS_LIST = SERVER_URL + "news/";
+	public static final String SENDID_NEWS_INFO = SERVER_URL + "news/index.php?Action=newsDetail";
+	public static final String SENDID_FLIYER_LIST = SERVER_URL + "flyer/";
+	public static final String SENDID_CTGRY = SERVER_URL + "category/";
+	public static final String SENDID_ITEM = SERVER_URL + "item/index.php?Action=itemList";
+	public static final String SENDID_ITEM_COUPON = SERVER_URL + "item/index.php?Action=itemList";
+	public static final String SENDID_ITEM_BARCODE = SERVER_URL + "item/index.php?Action=itemListBarcode";
+	public static final String SENDID_CPN_GIVE = SERVER_URL + "coupon/index.php?Action=giveList";
+	public static final String SENDID_CPN_TAKE = SERVER_URL + "coupon/index.php?Action=takedList";
+	public static final String SENDID_GET_CPN  = SERVER_URL + "coupon/index.php?Action=get";
+    public static final String SENDID_GET_QUES  = SERVER_URL + "fitting/";
+	public static final String PUSHSERVER_URL = SERVER_URL + "device_token/";
+	public static final String PASS_WORD = null;
+
+	public static final long scrollDelay = 4000;
+
+	/** loaderCount */
+	public static int loaderCnt = 0;
+	/** タブ画面で戻るボタン押下時の動作 */
+	public static final String BACK_STACK_KEY = "BACK_STACK";
+
+	static public List<MenuData> getMenuList(Context context) {
+		ArrayList<MenuData> dataList = new ArrayList<>();
+		dataList.add(new MenuData(0,R.drawable.icon_flyer,context.getResources().getString(R.string.flyer0)));
+		dataList.add(new MenuData(1,R.drawable.icon_infomation,context.getResources().getString(R.string.info)));
+		dataList.add(new MenuData(2,R.drawable.icon_shopping,context.getResources().getString(R.string.shopping)));
+		dataList.add(new MenuData(3,R.drawable.icon_coupon,context.getResources().getString(R.string.coupon)));
+		return dataList;
+	}
+
+    static {
+        boolean isSuccess = true;
+        HashMap<String, String> map = new HashMap<>();
+        try {
+            ArrayList<String> lineList = new ArrayList<>();
+
+            InputStream is = App.getContext().getResources().getAssets().open(
+                    "settingFile.txt");
+            InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+            BufferedReader br = new BufferedReader(isr);
+            String s;
+            while ((s = br.readLine()) != null) {
+                lineList.add(s);
+            }
+            br.close();
+            isr.close();
+            is.close();
+
+            for (String line : lineList) {
+                String[] values = line.split("=");
+                if (values[0] != null && !values[0].equals("")) {
+                    map.put(values[0], values[1]);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            isSuccess = false;
+
+        }
+        if (isSuccess) {
+            APP_ID = map.get("app_id");
+            APP_KEY = map.get("app_key");
+
+            int splashTime;
+            try {
+                splashTime = Integer.valueOf(map.get("splash_time"));
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+                splashTime = 2;
+            }
+            SPLASH_TIME = splashTime;
+        } else {
+            APP_ID = "pieceSample";
+            APP_KEY = "jokerpiece_appKey";
+            SPLASH_TIME = 2;
+        }
+    }
+
+}
