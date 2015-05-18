26513440d082f3acc26599754fd10ea46a3b74df
gm0907412 <kuo@jokerpiece.co.jp>
2015-05-18 14:03:06 +0900

    メモリオーバーとadapterlistView画像表示の不具合を修正->arr0.0.25

--- android.PieceCore/app/src/main/java/jp/co/jokerpiece/piecebase/util/BitmapCache.java
+++ android.PieceCore/app/src/main/java/jp/co/jokerpiece/piecebase/util/BitmapCache.java
@@ -1,10 +1,19 @@
 package jp.co.jokerpiece.piecebase.util;
 
 import android.graphics.Bitmap;
+import android.util.Log;
 import android.util.LruCache;
 
 public class BitmapCache {
-	 private LruCache<String, Bitmap> mMemoryCache;
+    private LruCache<String, Bitmap> mMemoryCache;
+    public static BitmapCache bitmapCache;
+
+    public static BitmapCache newInstance(){
+        if(bitmapCache == null){
+            bitmapCache = new BitmapCache();
+        }
+        return bitmapCache;
+    }
     BitmapCache(){
         int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
         int cacheSize = maxMemory / 8;       // 最大メモリに依存した実装
@@ -14,7 +23,13 @@ public class BitmapCache {
             protected int sizeOf(String key, Bitmap bitmap) {
                 return bitmap.getRowBytes() * bitmap.getHeight() / 1024;
             }
+            @Override
+            protected void entryRemoved(boolean evicted, String key, Bitmap oldBitmap, Bitmap newBitmap) {
+                oldBitmap.recycle();
+                oldBitmap = null;
+            }
         };
+
     }
 
     //保存したビットマップを取得
@@ -23,15 +38,20 @@ public class BitmapCache {
     }
 
     public void putBitmap(String url, Bitmap bitmap) {
-        Bitmap old = mMemoryCache.put(url,bitmap);
-        // オブジェクトの解放処理が必要なら以下のように実施
         synchronized (mMemoryCache) {
-            if (old != null){
-//                if(!old.isRecycled()){
-//                    old.recycle();
-//                }
-                old = null;
+            if (mMemoryCache.get(url) == null) {
+                mMemoryCache.put(url, bitmap);
             }
         }
+//        Bitmap old = mMemoryCache.put(url,bitmap);
+//        // オブジェクトの解放処理が必要なら以下のように実施
+//        synchronized (mMemoryCache) {
+//            if (old != null){
+////                if(!old.isRecycled()){
+////                    old.recycle();
+////                }
+//                old = null;
+//            }
+//        }
     }
 }
--- android.PieceCore/app/src/main/java/jp/co/jokerpiece/piecebase/util/DownloadImageSync.java
+++ android.PieceCore/app/src/main/java/jp/co/jokerpiece/piecebase/util/DownloadImageSync.java
@@ -5,75 +5,87 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v4.app.LoaderManager.LoaderCallbacks;
 import android.support.v4.content.Loader;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 
 
 
 public class DownloadImageSync implements LoaderCallbacks<Bitmap>{
-	static BitmapCache bmc = new BitmapCache();
-	Context context;
-	private String imageURL;
+    //static BitmapCache bmc = new BitmapCache();
+    Context context;
+    private String imageURL;
     private String tag;
     private ImageView imageView;
-	int position = -1;
-	DownloadImageSyncCallback callback;
+    int position = -1;
+    DownloadImageSyncCallback callback;
 
-	public DownloadImageSync(Context context,String imageURL,ImageView imageview,DownloadImageSyncCallback callback) {
-		this.context = context;
-		this.imageURL = imageURL;
-		this.imageView = imageview;
-		if(this.imageView != null && this.imageView.getTag() != null){
-			tag = this.imageView.getTag().toString();
-		}
-		this.callback = callback;
-	}
+    public DownloadImageSync(Context context,String imageURL,ImageView imageview,DownloadImageSyncCallback callback) {
+        this.context = context;
+        this.imageURL = imageURL;
+        this.imageView = imageview;
+        if(this.imageView != null && this.imageView.getTag() != null){
+            tag = this.imageView.getTag().toString();
+        }
+        this.callback = callback;
+    }
 
-	public boolean loadImageView(){
-		if(this.imageURL == null){
-			return false;
-		}
-		Bitmap data = bmc.getBitmap(this.imageURL);
-		if(data != null){
-			if(tag == null){
-				if(imageView != null){
-					imageView.setImageBitmap(data);
-				}
-			}else{
-				//Log.d("TAG", imageView.getTag().toString()+":"+tag);
-				callback.setImageCallbackWithURL(data, tag);
-			}
-			return true;
-		}
-		return false;
-	}
-	@Override
-	public Loader<Bitmap> onCreateLoader(int id, Bundle args) {
-		BitmapDownloader bmDownloader = new BitmapDownloader(context,imageURL);
-		bmDownloader.forceLoad();
-		return bmDownloader;
-	}
-	@Override
-	public void onLoadFinished(Loader<Bitmap> loader, Bitmap data) {
-		if(data != null){
-			if(tag == null){
-				if(imageView != null){
-					imageView.setImageBitmap(data);
-				}
-				bmc.putBitmap(imageURL, data);
-			}else{
-				//Log.d("TAG", imageView.getTag().toString()+":"+tag);
-				callback.setImageCallbackWithURL(data, tag);
-				bmc.putBitmap(tag, data);
-			}
-		}
-	}
-	@Override
-	public void onLoaderReset(Loader<Bitmap> loader) {
+    public boolean loadImageView(){
+        if(this.imageURL == null){
+            return false;
+        }
+        Bitmap data = BitmapCache.newInstance().getBitmap(this.imageURL);
+        if(data != null){
+            if(tag == null){
+                if(imageView != null){
+                    imageView.setImageBitmap(data);
+                }
+            }else{
+                //Log.d("TAG", imageView.getTag().toString()+":"+tag);
+                callback.setImageCallbackWithURL(data, tag);
+                if(imageView.getTag().toString().equals(tag)){
+                    imageView.setImageBitmap(data);
+                    imageView.setVisibility(View.VISIBLE);
+                }
+            }
+            return true;
+        }
+        return false;
+    }
 
-	}
+    @Override
+    public Loader<Bitmap> onCreateLoader(int id, Bundle args) {
+        BitmapDownloader bmDownloader = new BitmapDownloader(context,imageURL);
+        bmDownloader.forceLoad();
+        return bmDownloader;
+    }
+    @Override
+    public void onLoadFinished(Loader<Bitmap> loader, Bitmap data) {
+        if(data != null){
+            if(tag == null){
+                if(imageView != null){
+                    imageView.setImageBitmap(data);
+                }
+                BitmapCache.newInstance().putBitmap(imageURL, data);
+            }else{
+                //Log.d("TAG", imageView.getTag().toString() + ":" + tag);
+                callback.setImageCallbackWithURL(data, tag);
+                if(imageView.getTag().toString().equals(tag)){
+                    imageView.setImageBitmap(data);
+                    imageView.setVisibility(View.VISIBLE);
+                }
+                BitmapCache.newInstance().putBitmap(tag, data);
 
-	public interface DownloadImageSyncCallback{
-		public void setImageCallbackWithURL(Bitmap bitmap,String url);
-	}
+            }
+        }
+    }
+    @Override
+    public void onLoaderReset(Loader<Bitmap> loader) {
+
+    }
+
+    public interface DownloadImageSyncCallback{
+        public void setImageCallbackWithURL(Bitmap bitmap,String url);
+    }
 
 }
