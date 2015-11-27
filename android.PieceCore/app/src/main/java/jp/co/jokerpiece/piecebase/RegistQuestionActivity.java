7d4e57ab65dd148efeada711e817895bf9280f3d
j_itoh <itoh@jokerpiece.co.jp>
2015-11-27 20:16:58 +0900

    質問登録完了画面を表示するように変更

--- android.PieceCore/app/src/main/java/jp/co/jokerpiece/piecebase/RegistQuestionActivity.java
+++ android.PieceCore/app/src/main/java/jp/co/jokerpiece/piecebase/RegistQuestionActivity.java
@@ -21,15 +21,7 @@ public class RegistQuestionActivity extends Activity {
         WebView wv = (WebView) findViewById(R.id.webview);
         wv.getSettings().setJavaScriptEnabled(true);
         String url = Config.SENDID_REGIST_QUESTION + "?order_id=" + order_id + "&app_id=" + Config.APP_ID + "&app_key=" + Config.APP_KEY;
-        wv.setWebViewClient(new WebViewClient(){
-            @Override
-            public void onPageStarted(WebView view, String url, Bitmap favicon) {
-                super.onPageStarted(view, url, favicon);
-                if(url.endsWith(Config.QUESTION_FINISH)){
-                    finish();
-                }
-            }
-        });
+
         wv.loadUrl(url);
     }
 }
