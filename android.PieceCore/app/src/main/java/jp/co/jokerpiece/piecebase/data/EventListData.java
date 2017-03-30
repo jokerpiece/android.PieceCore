9383a8c250b9f6b21bcd3c342633b4062c443722
Sou <sou@jokerpiece.co.jp>
2017-03-30 15:37:03 +0900

    DetailReserveAPIとEventListDataのファイル名変更

---
+++ android.PieceCore/app/src/main/java/jp/co/jokerpiece/piecebase/data/EventListData.java
@@ -0,0 +1,17 @@
+package jp.co.jokerpiece.piecebase.data;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by Antifuture on 2016/9/27.
+ */
+
+public class EventListData
+{
+    public String error_code;
+    public String error_message;
+
+    public ArrayList<HashMap<String, String>> reserveList;
+    public ArrayList<HashMap<String, String>> eventList;
+}
